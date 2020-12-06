a9489ed0b3551005d8234a146f67f09ed771584a
hgunesdogdu <hande.gunesdogdu@hepsiburada.com>
2020-12-06 15:07:21 +0300

    first commit, rabbitmq consumer created

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func failOnError(err error, errMessage string) {
+	if err != nil {
+		log.Fatalf("%s: %s", errMessage, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	//declare the queue from which we're going to consume
+	q, err := ch.QueueDeclare(
+		"InsertProduct",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
